d179b5c5c0ab930242be0067fcd20a26facf59b0
luizfelipebraga <ruivolfb@gmail.com>
2021-10-03 22:16:52 -0300

    fix: add begginer typescript project

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 
 const port = 3335;
 
 const app = express();
 app.use(express.json());
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.listen(port, () => console.log("listening on port " + port));
