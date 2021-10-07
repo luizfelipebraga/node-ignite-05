547bb0fae9fa3cc4287081947f5862f1e0fefb96
luizfelipebraga <ruivolfb@gmail.com>
2021-10-07 17:06:17 -0300

    docs(readme): add solid principles

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-ignite-05
+
+## Principios S.O.L.I.D 
+S => SRP - Single Responsability Principle
+O => OCP - Open-Closed-Principle   
+L => LSP - Liskov Substituion Principle
+I => ISP - Interface Segregation Principle 
+D => DIP - Dependecy Inversion Principle
--- rentalx/src/services/CategoryService.ts
+++ rentalx/src/services/CategoryService.ts
@@ -12,12 +12,13 @@ export class CategoryService {
         this.categoryRepository = categoryRepository;
     }
 
+    //This way, we dont need to do what i did on top -->> Dependecy Inversion Principle //**constructor(private categoryRepository: CategoryRepository) {}**//
+
     execute({ name, description }: ICategoryService): void {
         const categoryAlreadyExists = this.categoryRepository.findByName(name);
 
         if (categoryAlreadyExists) {
             throw new Error("Category Already Exists");
-            
         }
 
         this.categoryRepository.create({ name, description});
