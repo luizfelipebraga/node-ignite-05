27c8409f4505c5ca9dfda87268b1c0316913920c
Luiz Felipe Braga <ruivolfb@gmail.com>
2022-01-13 17:33:03 -0300

    add swagger.json file

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 const port = process.env.PORT || 3333;
 
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
