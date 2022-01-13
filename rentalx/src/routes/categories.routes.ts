1cd094d933cef10a92727977e97c70f77815c3b3
luizfelipebraga <ruivolfb@gmail.com>
2022-01-12 21:34:00 -0300

    Add multer middleware (routes categories)

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/category/create";
 import { listCategoryController } from "../modules/cars/useCases/category/list";
 
 export const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/categories", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,4 +15,10 @@ categoriesRoutes.post("/categories", (request, response) => {
 
 categoriesRoutes.get("/categories", (request, response) => {
   return listCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
